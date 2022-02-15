5192f1deb415f124cab3b3313c2253467e5d5cfb
Deepashri Dali <Deepashri.Dali@softwire.com>
2022-02-15 12:08:43 +0000

    Initial Commit

---
+++ src/components/go-roving/goRovingPhotos/GoRovingPhotoListPage.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+export function GoRovingPhotoListPage() {
+  const [photoList, setPhotoList] = useState();
+
+  return (
+    <main>
+      <h1></h1>
+    </main>
+  );
+}
