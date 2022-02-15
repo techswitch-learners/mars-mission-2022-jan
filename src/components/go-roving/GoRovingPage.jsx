42c2fbadae8032602109d76c183014f1fb897241
Alison Thomson <alisonmcknight@hotmail.com>
2022-02-15 11:36:24 +0000

    MM-12: Add 3 links to Go Roving page

--- src/components/go-roving/GoRovingPage.jsx
+++ src/components/go-roving/GoRovingPage.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+import react from "react";
+
 export const GoRovingPage = () => {
   return (
     <main>
       <h1>Go Roving!</h1>
       <p>Mars Mission Go Roving! page</p>
+      <Link to="/go-roving/curiosity">
+        <h2>Curiosity</h2>
+      </Link>
+      <Link to="/go-roving/opportunity">
+        <h2>Opportunity</h2>
+      </Link>
+      <Link to="/go-roving/spirit">
+        <h2>Spirit</h2>
+      </Link>
     </main>
   );
 };
