e1260bce6cd1d6b5e73862cc33c20fa9266183d6
Sam James <Sam.James@softwire.com>
2022-02-15 17:52:52 +0000

    MM-17: Replace main tags with div, remove unnessary elements from component

--- src/components/asteroid-display/AsteroidDisplay.jsx
+++ src/components/asteroid-display/AsteroidDisplay.jsx
@@ -2,18 +2,11 @@ import React from "react";
 
 export function AsteroidDisplay() {
   return (
-    <main>
-      <h1>Asteroids Near Earth...</h1>
-      <h2>Diameter:</h2>
-      <p></p>
-      <h2>Veloicty:</h2>
-      <p></p>
-      <h2>Distance:</h2>
-      <p></p>
+    <div className="asteroid-display-content">
       <img
         src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2015/04/14/12/asteroid1404a.jpg?width=968&auto=webp&quality=50&crop=968%3A645%2Csmart"
         alt="Meteor image"
       />
-    </main>
+    </div>
   );
 }
