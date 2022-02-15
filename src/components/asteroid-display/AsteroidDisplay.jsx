3c0da6d4735cac5a81aaaf067c468d67d4d70f22
Sam James <Sam.James@softwire.com>
2022-02-15 15:52:47 +0000

    MM-17: Add details to Asteroid display

--- src/components/asteroid-display/AsteroidDisplay.jsx
+++ src/components/asteroid-display/AsteroidDisplay.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-export function AsteroidDisplay() {
-  return <main></main>;
+export function AsteroidDisplay(diameter, velocity, distance) {
+  return (
+    <main>
+      <h2>Diameter:</h2>
+      <p>{diameter}</p>
+      <h2>Veloicty:</h2>
+      <p>{velocity}</p>
+      <h2>Distance:</h2>
+      <p>{distance}</p>
+      <img src="" alt="" />
+    </main>
+  );
 }
