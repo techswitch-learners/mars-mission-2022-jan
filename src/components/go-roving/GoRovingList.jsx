d3bac4ca644bd74263f22ee11fa5916d6a4ea9d2
Ija Saporenkova <Ija.Saporenkova@softwire.com>
2022-02-16 14:18:25 +0000

    MM-10: Add rover component and change tests

--- src/components/go-roving/GoRovingList.jsx
+++ src/components/go-roving/GoRovingList.jsx
@@ -1,39 +1,14 @@
 import react from "react";
-import { Link } from "react-router-dom";
+import { GoRovingRover } from "./GoRovingRover";
 
-export function GoRovingList() {
+export function GoRovingList({ rovers }) {
   return (
     <div className="rover-list">
-      <div className="rover">
-        <h2 className="rover-name">Opportunity</h2>
-        <Link to="/go-roving/opportunity">
-          <img
-            className="rover-image"
-            src="https://www.issnationallab.org/wp-content/uploads/2018/05/opportunity-rover-square.jpg"
-            alt="Opportunity Rover"
-          />
-        </Link>
-      </div>
-      <div className="rover">
-        <h2 className="rover-name">Curiosity</h2>
-        <Link to="/go-roving/curiosity">
-          <img
-            className="rover-image"
-            src="https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/pia23378-16.jpg"
-            alt="Curiosity Rover"
-          />
-        </Link>
-      </div>
-      <div className="rover">
-        <h2 className="rover-name">Spirit</h2>
-        <Link to="/go-roving/spirit">
-          <img
-            className="rover-image"
-            src="http://searchoflife.com/wp-content/uploads/2014/06/mars-spirit-rover.jpg"
-            alt="Spirit Rover"
-          />
-        </Link>
-      </div>
+      {rovers.map((rover, index) => (
+        <div className="rover" key={index}>
+          <GoRovingRover rover={rover} />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/go-roving/GoRovingList.test.js
+++ src/components/go-roving/GoRovingList.test.js
@@ -2,38 +2,17 @@ import { render, screen } from "@testing-library/react";
 import { GoRovingList } from "./GoRovingList";
 import { BrowserRouter } from "react-router-dom";
 
-test("Go Roving page renders without error", () => {
+test("Go Roving list page renders without error", () => {
+  let rovers = [
+    {
+      name: "Opportunity",
+      link: "/go-roving/opportunity",
+      img: "https://www.issnationallab.org/wp-content/uploads/2018/05/opportunity-rover-square.jpg",
+    },
+  ];
   render(
     <BrowserRouter>
-      <GoRovingList />
+      <GoRovingList rovers={rovers} />
     </BrowserRouter>
   );
-});
-
-test("Go Roving page contains the heading opportunity", () => {
-  render(
-    <BrowserRouter>
-      <GoRovingList />
-    </BrowserRouter>
-  );
-  const titleElements = screen.getAllByText(/opportunity/i);
-  expect(titleElements[0]).toBeInTheDocument();
-});
-test("Go Roving page contains the heading curiosity", () => {
-  render(
-    <BrowserRouter>
-      <GoRovingList />
-    </BrowserRouter>
-  );
-  const titleElements = screen.getAllByText(/curiosity/i);
-  expect(titleElements[0]).toBeInTheDocument();
-});
-test("Go Roving page contains the heading spirit", () => {
-  render(
-    <BrowserRouter>
-      <GoRovingList />
-    </BrowserRouter>
-  );
-  const titleElements = screen.getAllByText(/spirit/i);
-  expect(titleElements[0]).toBeInTheDocument();
 });
