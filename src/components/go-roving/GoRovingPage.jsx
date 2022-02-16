9a906ae942d7586ff3c2003f5febd5b9c3504eda
Ija Saporenkova <Ija.Saporenkova@softwire.com>
2022-02-16 11:00:57 +0000

    MM-28: Add responsive design styling to go rover page

--- src/components/go-roving/GoRovingPage.jsx
+++ src/components/go-roving/GoRovingPage.jsx
@@ -6,30 +6,38 @@ export const GoRovingPage = () => {
   return (
     <main>
       <h1>Go Roving!</h1>
-      <h2 className="rover-name">Opportunity</h2>
-      <Link to="/go-roving/opportunity">
-        <img
-          className="rover-image"
-          src="https://www.issnationallab.org/wp-content/uploads/2018/05/opportunity-rover-square.jpg"
-          alt="Opportunity Rover"
-        />
-      </Link>
-      <h2 className="rover-name">Curiosity</h2>
-      <Link to="/go-roving/curiosity">
-        <img
-          className="rover-image"
-          src="https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/pia23378-16.jpg"
-          alt="Curiosity Rover"
-        />
-      </Link>
-      <h2 className="rover-name">Spirit</h2>
-      <Link to="/go-roving/spirit">
-        <img
-          className="rover-image"
-          src="https://www.spaceflightinsider.com/wp-content/uploads/2015/01/Mars-Exploration-Rover-Spirit-Opportunity-surface-of-Red-Planet-NASA-image-posted-on-SpaceFlight-Insider-300x240.jpg"
-          alt="Spirit Rover"
-        />
-      </Link>
+      <div className="rover-list">
+        <div className="rover">
+          <h2 className="rover-name">Opportunity</h2>
+          <Link to="/go-roving/opportunity">
+            <img
+              className="rover-image"
+              src="https://www.issnationallab.org/wp-content/uploads/2018/05/opportunity-rover-square.jpg"
+              alt="Opportunity Rover"
+            />
+          </Link>
+        </div>
+        <div className="rover">
+          <h2 className="rover-name">Curiosity</h2>
+          <Link to="/go-roving/curiosity">
+            <img
+              className="rover-image"
+              src="https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/pia23378-16.jpg"
+              alt="Curiosity Rover"
+            />
+          </Link>
+        </div>
+        <div className="rover">
+          <h2 className="rover-name">Spirit</h2>
+          <Link to="/go-roving/spirit">
+            <img
+              className="rover-image"
+              src="http://searchoflife.com/wp-content/uploads/2014/06/mars-spirit-rover.jpg"
+              alt="Spirit Rover"
+            />
+          </Link>
+        </div>
+      </div>
     </main>
   );
 };
