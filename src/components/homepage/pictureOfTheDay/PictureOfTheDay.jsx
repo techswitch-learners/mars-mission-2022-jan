424e0f14a88cb962a2657f05868d6675b91e489f
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-02-16 15:33:08 +0000

    MM-26: Add conditional rendering to PictureOfTheDay function so that, if media type is a video, the video will render

--- src/components/homepage/pictureOfTheDay/PictureOfTheDay.jsx
+++ src/components/homepage/pictureOfTheDay/PictureOfTheDay.jsx
@@ -6,22 +6,29 @@ export function PictureOfTheDay() {
   const [imageUrl, setImageUrl] = useState();
   const [imageTitle, setImageTitle] = useState();
   const [imageCaption, setImageCaption] = useState();
+  const [imageType, setImageType] = useState();
 
   useEffect(function () {
     getPotdData().then((potd) => {
       setImageUrl(potd.url);
       setImageTitle(potd.title);
       setImageCaption(potd.explanation);
+      setImageType(potd.media_type);
     });
   }, []);
 
   return (
     <section>
       <h1>NASA Astronomy Picture of the Day: {imageTitle}</h1>
-      <img src={imageUrl}></img>
-      <p>
-        <ReactPlayer url="https://www.youtube.com/embed/liapnqj9GDc?rel=0" />
-      </p>
+
+      {imageType === "video" && (
+        <p>
+          <ReactPlayer url={imageUrl} />
+        </p>
+      )}
+
+      {imageType === "image" && <img src={imageUrl}></img>}
+
       <figcaption>{imageCaption}</figcaption>
     </section>
   );
