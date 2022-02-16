eb414b5044ed8623066825816b22a9f4bc6ddc5a
Sam James <Sam.James@softwire.com>
2022-02-16 11:06:29 +0000

    MM-17: Restructure component with correct containers and change image to comet

--- src/components/asteroids/AsteroidsDisplay/AsteroidsDisplay.jsx
+++ src/components/asteroids/AsteroidsDisplay/AsteroidsDisplay.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 export function AsteroidsDisplay() {
   return (
-    <div className="asteroid-display-content">
-      <img
-        src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2015/04/14/12/asteroid1404a.jpg?width=968&auto=webp&quality=50&crop=968%3A645%2Csmart"
-        alt="Meteor image"
-      />
-    </div>
+    <section>
+      <div className="asteroid-display-content"></div>
+      <div className="image">
+        <img
+          src="https://images.newscientist.com/wp-content/uploads/2020/06/04160433/r4500292-comet_ikeya-zhang_andromeda_galaxy_optical_web.jpg"
+          alt="Meteor image"
+        />
+      </div>
+    </section>
   );
 }
