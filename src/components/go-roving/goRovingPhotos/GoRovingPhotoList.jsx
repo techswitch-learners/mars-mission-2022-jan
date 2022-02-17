a91c8cf0b0f2d3c5642b5de66027880a04566460
Deepashri Dali <Deepashri.Dali@softwire.com>
2022-02-17 12:31:53 +0000

    MM-15:Add thumbnail images and create responsive design

--- src/components/go-roving/goRovingPhotos/GoRovingPhotoList.jsx
+++ src/components/go-roving/goRovingPhotos/GoRovingPhotoList.jsx
@@ -1,16 +1,22 @@
-export function GoRovingPhotoList({ photos, onClick }) {
+import "./GoRovingPhotoList.scss";
+
+export function GoRovingPhotoList({ photos, onClick, selectedPhoto }) {
   return (
-    <ul>
-      <li>
-        {photos.map((photo) => (
+    <div className="image-selector">
+      {photos.map((photo) => (
+        <div
+          className={`image-selector__thumbnail ${
+            selectedPhoto === photo ? "selected" : ""
+          }`}
+        >
           <img
             key={photo.id}
             src={photo.img_src}
             alt=""
             onClick={() => onClick(photo)}
           />
-        ))}
-      </li>
-    </ul>
+        </div>
+      ))}
+    </div>
   );
 }
