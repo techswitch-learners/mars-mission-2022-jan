825dc9eeaf3613fec7508609182ef9ea1fb737b5
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-02-17 10:24:09 +0000

    MM-28: Add useEffect

--- src/components/asteroids/AsteroidDisplay/DatePicker/DatePicker.jsx
+++ src/components/asteroids/AsteroidDisplay/DatePicker/DatePicker.jsx
@@ -4,28 +4,29 @@ import { getAsteroids } from "../../../../clients/asteroidClient";
 export function DatePicker() {
   const [fromDate, setFromDate] = useState("2022-02-01");
   const [toDate, setToDate] = useState("2022-02-07");
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
+  const [asteroidData, setAsteroidData] = useState();
 
-  const [asteroidData, setAsteroidData] = useState(
-    getAsteroids(fromDate, toDate)
+  useEffect(
+    function () {
+      setLoading(true);
+      getAsteroids(fromDate, toDate).then((asteroid) => {
+        setAsteroidData(asteroid);
+        setLoading(false);
+      });
+    },
+    [fromDate, toDate]
   );
-
-  let asteroidCount;
-  if (!asteroidData) {
-    asteroidCount = "Loading asteroids...";
-  } else {
-    asteroidCount = asteroidData.element_count;
-  }
 
   return (
     <section>
       <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          setLoading(true);
-          setAsteroidData(await getAsteroids(fromDate, toDate));
-          setLoading(false);
-        }}
+      // onSubmit={async (e) => {
+      //   e.preventDefault();
+      //   setLoading(true);
+      //   setAsteroidData(await getAsteroids(fromDate, toDate));
+      //   setLoading(false);
+      // }}
       >
         <label htmlFor="fromDate">from:</label>
         <input
@@ -42,12 +43,12 @@ export function DatePicker() {
           value={toDate}
           onChange={(e) => setToDate(e.target.value)}
         />
-        <input type="submit" />
+        {/* <input type="submit" /> */}
       </form>
       <div className="asteroid-info">
         <h2>
-          Number of asteroids near earth:{" "}
-          {loading ? "Loading asteroids..." : asteroidCount}{" "}
+          Number of asteroids near earth:
+          {loading ? "Loading asteroids..." : asteroidData.element_count}
         </h2>
       </div>
     </section>
