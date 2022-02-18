bfdcd33c128847abcc35d0e5d890bb5add2ce351
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-02-18 13:55:55 +0000

    MM-22: Made changes according to review comments

--- src/components/asteroids/Asteroid/Asteroid.jsx
+++ src/components/asteroids/Asteroid/Asteroid.jsx
@@ -9,19 +9,19 @@ export function Asteroid({ asteroidData, date }) {
         asteroidData.near_earth_objects[[date]][0].estimated_diameter.kilometers
           .estimated_diameter_max
       )
-    : "none";
+    : undefined;
   const VelocityByFormulaOneCar = asteroidData.near_earth_objects[[date]]
     ? getVelocityByFormulaOneCar(
         asteroidData.near_earth_objects[[date]][0]?.close_approach_data[0]
           .relative_velocity.kilometers_per_second
       )
-    : "none";
+    : undefined;
   const DistanceFromEarthByShard = asteroidData.near_earth_objects[[date]]
     ? getDistanceFromEarthByShard(
         asteroidData.near_earth_objects[[date]][0]?.close_approach_data[0]
           .miss_distance.kilometers
       )
-    : "none";
+    : undefined;
   return (
     <div className="asteroid-info">
       <h2>Asteroids near earth: </h2>
