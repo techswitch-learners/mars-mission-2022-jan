b0474a4324066be8339f59746fa66d150712eed9
Sam James <Sam.James@softwire.com>
2022-02-18 12:46:44 +0000

    MM-19: Make sure to catch if asteroid data returns undefined

--- src/components/asteroids/Asteroid/Asteroid.jsx
+++ src/components/asteroids/Asteroid/Asteroid.jsx
@@ -4,18 +4,24 @@ import { getVelocityByFormulaOneCar } from "../../../helperFunctions/converterFu
 import { getDistanceFromEarthByShard } from "../../../helperFunctions/converterFunctions";
 
 export function Asteroid({ asteroidData, date }) {
-  const diameterInFootballPitches = getDiameterInFootballPitches(
-    asteroidData.near_earth_objects[[date]][0].estimated_diameter.kilometers
-      .estimated_diameter_max
-  );
-  const VelocityByFormulaOneCar = getVelocityByFormulaOneCar(
-    asteroidData.near_earth_objects[[date]][0].close_approach_data[0]
-      .relative_velocity.kilometers_per_second
-  );
-  const DistanceFromEarthByShard = getDistanceFromEarthByShard(
-    asteroidData.near_earth_objects[[date]][0].close_approach_data[0]
-      .miss_distance.kilometers
-  );
+  const diameterInFootballPitches = asteroidData.near_earth_objects[[date]]
+    ? getDiameterInFootballPitches(
+        asteroidData.near_earth_objects[[date]][0].estimated_diameter.kilometers
+          .estimated_diameter_max
+      )
+    : "none";
+  const VelocityByFormulaOneCar = asteroidData.near_earth_objects[[date]]
+    ? getVelocityByFormulaOneCar(
+        asteroidData.near_earth_objects[[date]][0]?.close_approach_data[0]
+          .relative_velocity.kilometers_per_second
+      )
+    : "none";
+  const DistanceFromEarthByShard = asteroidData.near_earth_objects[[date]]
+    ? getDistanceFromEarthByShard(
+        asteroidData.near_earth_objects[[date]][0]?.close_approach_data[0]
+          .miss_distance.kilometers
+      )
+    : "none";
   return (
     <div className="asteroid-info">
       <h2>Asteroids near earth: </h2>
@@ -23,7 +29,7 @@ export function Asteroid({ asteroidData, date }) {
         "There are no asteroids for the chosen day!"
       ) : (
         <div className="asteroid-container">
-          <p>{asteroidData.element_count} total asteroids near earth:</p>
+          <p>{asteroidData?.element_count} total asteroids near earth:</p>
           <h2>Nearest Asteroid is ...</h2>
 
           <p>
