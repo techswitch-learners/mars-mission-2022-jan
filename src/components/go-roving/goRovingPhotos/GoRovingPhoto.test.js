cf4ff0059aaa3fc9ed2e4cc311b88299e94d39f6
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-02-18 14:01:14 +0000

    MM-15-Tests: Remove redundant import line

--- src/components/go-roving/goRovingPhotos/GoRovingPhoto.test.js
+++ src/components/go-roving/goRovingPhotos/GoRovingPhoto.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import renderer from "react-test-renderer";
 import { GoRovingPhoto } from "./GoRovingPhoto";
 
 test("Camera full name is correct", () => {
