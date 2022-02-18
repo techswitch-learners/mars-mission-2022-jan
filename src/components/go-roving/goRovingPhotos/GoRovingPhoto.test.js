45d0f69e729f2e3e9837057b6d30f9e05203f36c
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-02-18 13:41:15 +0000

    MM-15-Tests: Add test for photo component

---
+++ src/components/go-roving/goRovingPhotos/GoRovingPhoto.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import { GoRovingPhoto } from "./GoRovingPhoto";
+
+test("Camera full name is correct", () => {
+  const photo = {
+    img_src: "some image url",
+    camera: { full_name: "some camera" },
+  };
+
+  render(<GoRovingPhoto photo={photo} />);
+  const element = screen.getByText(/some camera/);
+
+  expect(element).toBeInTheDocument();
+});
+
+test("Image is correct", () => {
+  const photo = {
+    img_src: "some image url",
+    camera: { name: "some camera" },
+  };
+
+  render(<GoRovingPhoto photo={photo} />);
+  const element = screen.getByRole("img");
+
+  expect(element).toBeInTheDocument();
+});
+
+test("Image alt is correct", () => {
+  const photo = {
+    img_src: "some image url",
+    camera: { name: "some camera" },
+  };
+
+  render(<GoRovingPhoto photo={photo} />);
+  const element = screen.getByRole("img", "From Rover");
+
+  expect(element).toBeInTheDocument();
+});
