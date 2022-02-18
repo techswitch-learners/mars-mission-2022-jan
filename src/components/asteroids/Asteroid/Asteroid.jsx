c84058b2c2f5df173990e5ca24db2e26fa80d5b6
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-02-18 11:28:57 +0000

    MM-19: Implement functions to asteroid component

--- src/components/asteroids/Asteroid/Asteroid.jsx
+++ src/components/asteroids/Asteroid/Asteroid.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { getDiameterInFootballPitches } from "../../../helperFunctions/converterFunctions";
+import { getVelocityByFormulaOneCar } from "../../../helperFunctions/converterFunctions";
+import { getDistanceFromEarthByShard } from "../../../helperFunctions/converterFunctions";
 
 export function Asteroid({ asteroidData, date }) {
+  const diameterInMeters = getDiameterInFootballPitches(
+    asteroidData.near_earth_objects[[date]][0].estimated_diameter.kilometers
+      .estimated_diameter_max
+  );
+  const velocityByMeter = getVelocityByFormulaOneCar(
+    asteroidData.near_earth_objects[[date]][0].close_approach_data[0]
+      .relative_velocity.kilometers_per_second
+  );
+  const distanceInKm = getDistanceFromEarthByShard(
+    asteroidData.near_earth_objects[[date]][0].close_approach_data[0]
+      .miss_distance.kilometers
+  );
   return (
     <div className="asteroid-info">
       <h2>Asteroids near earth: </h2>
@@ -11,29 +26,21 @@ export function Asteroid({ asteroidData, date }) {
           <p>
             {asteroidData.element_count} total asteroids near earth on {date}
           </p>
-          <p>Nearest Asteroid = </p>
+          <p>Nearest Asteroid is ...</p>
 
           <p>
-            {
-              asteroidData.near_earth_objects[[date]][0].estimated_diameter
-                .kilometers.estimated_diameter_max
-            }
-            km in diameter
+            {diameterInMeters + " "}
+            football pitches wide!
           </p>
           <p>
-            travelling at
-            {
-              asteroidData.near_earth_objects[[date]][0].close_approach_data[0]
-                .relative_velocity.kilometers_per_second
-            }
-            km per second.
+            Travelling
+            {" " + velocityByMeter + " "}
+            times faster than a formula1 car!
           </p>
           <p>
-            {
-              asteroidData.near_earth_objects[[date]][0].close_approach_data[0]
-                .miss_distance.kilometers
-            }
-            km away from the earth
+            Stack up the Shard
+            {" " + distanceInKm + " "}
+            times! That's how far away it is.
           </p>
         </>
       )}
